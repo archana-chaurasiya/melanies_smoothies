5b62f6d6a5ad0b12d643d97a56401cf343a26373
Archana <archana.chaurasiya@gmail.com>
2025-03-27 11:21:27 +0530

    Update streamlit_app.py
    Added a new section to display smoothies fruit nutretion information and new requests package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 
 
 # Write directly to the app
@@ -40,5 +45,6 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert and ingredients_string:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, ' + name_on_order+'!', icon="✅")
+
 
 
